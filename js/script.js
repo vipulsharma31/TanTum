c3a0ab8c25f2fbe626340d91cb80256c016af031
vipulsharma31 <114390833+vipulsharma31@users.noreply.github.com>
2023-03-12 22:04:01 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".reviews-slider", {
+    grabCursor:true,
+    loop:true,
+    autoHeight:true,
+    spaceBetween: 20,
+    breakpoints: {
+       0: {
+         slidesPerView: 1,
+       },
+       700: {
+         slidesPerView: 2,
+       },
+       1000: {
+         slidesPerView: 3,
+       },
+    },
+ });
+ 
+
+ $(document).ready(function() {
+  $('.animated').addClass('fadeIn');
+});
